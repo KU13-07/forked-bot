a434e42159e30b0aa82dc3278c744c27d14bd9d4
KU13 <lionplays1062@Gmail.com>
2023-03-27 14:52:41 -0400

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -3,6 +3,9 @@ from discord.ext.pages import Paginator
 import asyncio
 from yt_dlp import YoutubeDL
 from discord import ApplicationContext
+import time
+
+TIMEOUT = 300
 
 class Source():
     YDL_OPTS = {
@@ -16,24 +19,30 @@ class Source():
         try:
             with YoutubeDL(self.YDL_OPTS) as ydl:
                 data = ydl.extract_info(search, download=False)
+                self.sort_data(data)
         except:
             raise discord.ApplicationCommandError("Invalid URL")
 
+    def create_embed(self) -> discord.Embed:
+        embed = (discord.Embed(title=self.title,
+                                    url=self.video_url)
+                 .set_author(name=self.uploader, url=self.uploader_url)
+                 .set_image(url=self.thumbnail)
+                 .add_field(name='Duration', value=f"{self.duration}s"))
+        return embed
+        
+    def sort_data(self, data: dict):
         if data.get("entries"):
             data = data["entries"][0]
 
-        self.title = data.get("title")
-        self.url = data.get("webpage_url")
-        self.uploader = data.get("uploader")
-        self.uploader_url = data.get("uploader_url")
-        self.thumbnail = data.get("thumbnail")
-        self.duration = data.get("duration")
-        self.url = data.get("url")
+        self.title = data["title"]
+        self.video_url = data["webpage_url"]
+        self.uploader = data["uploader"]
+        self.uploader_url = data["uploader_url"]
+        self.thumbnail = data["thumbnail"]
+        self.duration = data["duration"]
+        self.url = data["url"]
         self.start_time = data.get("start_time") # Not always
-
-        self.embed = discord.Embed(title=self.title, url=self.url)
-        self.embed.set_author(name=self.uploader, url=self.uploader_url)
-        self.embed.set_image(url=self.thumbnail)
 
 class VoiceState:
     def __init__(self, bot: discord.Bot):
@@ -44,24 +53,31 @@ class VoiceState:
         self.ctx = None
 
         self.next = asyncio.Event()
+        self.events = []
         self.current = None
         self.player = bot.loop.create_task(self.audio_player_task())
-    
+
     @property
     def is_playing(self):
-        return bool(self.current)
+        return self.voice.is_paused() or self.voice.is_playing()
 
     def after(self, arg):
         self.next.set()
 
     async def audio_player_task(self):
         while True:
-            self.next.clear()
-
             if not self.loop:
-                self.current = await self.queue.get()
+                try:
+                    async with asyncio.timeout(TIMEOUT):
+                        self.current = await self.queue.get()
+                except asyncio.TimeoutError:
+                    self.queue = asyncio.Queue()
+                    await self.voice.disconnect()
 
-            embed = self.current.embed
+            self.next.clear()
+            self.events = [time.time()]
+
+            embed = self.current.create_embed()
             embed.set_footer(text="Now playing")
 
             source = discord.FFmpegPCMAudio(self.current.url)
@@ -78,11 +94,11 @@ class Music(discord.Cog):
         self.bot = bot
         self.voice_states = {}
 
-    async def cog_command_error(self, ctx: ApplicationContext, error: Exception):
-        if str(error) == "Application Command raised an exception: AttributeError: 'NoneType' object has no attribute 'channel'":
-            error = "Bot is not currently in use"
-        embed = discord.Embed(title='Error', description=error)
-        await ctx.respond(embed=embed)
+    # async def cog_command_error(self, ctx: ApplicationContext, error: Exception):
+    #     if str(error) == "Application Command raised an exception: AttributeError: 'NoneType' object has no attribute 'channel'":
+    #         error = "Bot is not currently in use"
+    #     embed = discord.Embed(title='Error', description=error)
+    #     await ctx.respond(embed=embed)
 
     def get_voice_state(self, ctx: ApplicationContext):
         voice_state = self.voice_states.get(ctx.guild_id)
@@ -153,47 +169,74 @@ class Music(discord.Cog):
     async def _leave(self, ctx):
         await self._disconnect(ctx)
 
-    @discord.command()
-    async def skip(self, ctx: ApplicationContext):
+    @discord.command(name="skip")
+    async def _skip(self, ctx: ApplicationContext):
         destination = ctx.author.voice.channel
         if destination != ctx.voice_state.voice.channel:
             raise discord.ApplicationCommandError("You must be in same voice channel as bot to use this command")            
 
         ctx.voice_state.voice.stop()
 
-        embed = ctx.voice_state.current.embed
+        embed = ctx.voice_state.current.create_embed()
         embed.set_footer(text="Skipped")
         await ctx.respond(embed=embed)
  
-    @discord.command()
+    @discord.command(name="volume")
     @discord.option("value", description="Volume value", min_value=0, max_value=2, required=True)
-    async def volume(self, ctx: ApplicationContext, value: int):
+    async def _volume(self, ctx: ApplicationContext, value: int):
         voice_state = self.get_voice_state(ctx)
         voice_state.volume = value
 
-    @discord.command()
-    async def loop(self, ctx: ApplicationContext):
+    @discord.command(name="loop")
+    async def _loop(self, ctx: ApplicationContext):
         await self.check(ctx)
 
-        embed = ctx.voice_state.current.embed
+        embed = ctx.voice_state.current.create_embed()
         if ctx.voice_state.loop: # => no longer looped
             embed.set_footer(text="No longer looping")
         else:
             embed.set_footer(text="Now looping")
         ctx.voice_state.loop = not ctx.voice_state.loop
         await ctx.respond(embed=embed)
+
+    @discord.command(name="progress")
+    async def _progress(self, ctx: ApplicationContext):
+        await self.check(ctx)
+        events = ctx.voice_state.events
         
+        # idek stolen from chat-gpt
+        total = 0
+        for i in range(0, len(events), 2):
+            start_time = events[i]
+            end_time = events[i+1] if i+1 < len(events) else time.time()
+            total += end_time - start_time
+
+        embed = ctx.voice_state.current.create_embed()
+        diff = int(total/ctx.voice_state.current.duration*10)
+        embed.description = "⬜"*diff+"⬛"*(10-diff)
+        embed.set_footer(text=f"Currently playing | 1/{ctx.voice_state.queue.qsize()+1}")
+
+        await ctx.respond(embed=embed)
+    @discord.command(name="current")
+    async def _current(self, ctx: ApplicationContext):
+        await self._progress(ctx)
+    @discord.command(name="duration")
+    async def _duration(self, ctx: ApplicationContext):
+        await self._progress(ctx)
 
     @discord.command(name="queue")
     async def _queue(self, ctx: ApplicationContext):
         await self.check(ctx)
-        queue = list(ctx.voice_state.queue._queue)
-        for num, song in enumerate(queue):
-            song.embed.set_footer(text=f"{num+2}/{len(queue)+1}")
-        ctx.voice_state.current.embed.set_footer(text=f"Currently playing | 1/{len(queue)+1}")
-        queue = [ctx.voice_state.current] + queue
+        queue = ctx.voice_state.queue._queue
 
-        paginator = Paginator(pages=[source.embed for source in queue])
+        pages = [(ctx.voice_state.current.create_embed()
+                 .set_footer(text=f"Currently playing | 1/{len(queue)+1}"))]
+        
+        for num, song in enumerate(list(queue)):
+            pages.append((song.create_embed()
+                          .set_footer(text=f"{num+2}/{len(queue)+1}")))
+       
+        paginator = Paginator(pages=pages)
         await paginator.respond(ctx.interaction, ephemeral=False)
     @discord.command(name="list")
     async def _list(self, ctx):
@@ -204,10 +247,16 @@ class Music(discord.Cog):
         await self.check(ctx)
         voice = ctx.voice_state.voice
 
+        ctx.voice_state.events.append(time.time())
+
+        embed = ctx.voice_state.current.embed
         if voice.is_paused():
             voice.resume()
+            embed.set_footer(text="Now playing")
         else:
             voice.pause()
+            embed.set_footer(text="Now paused")
+        await ctx.respond(embed=embed)
     @discord.command(name="resume")
     async def _resume(self, ctx: ApplicationContext):
         await self._pause(ctx)
@@ -215,6 +264,7 @@ class Music(discord.Cog):
     @discord.command(name="play")
     @discord.option("search", description="Search for song", required=True)
     async def _play(self, ctx: ApplicationContext, search: str):
+        start = time.perf_counter()
         await self.connect(ctx)
         await ctx.defer()
 
@@ -241,6 +291,6 @@ class Music(discord.Cog):
     @discord.command(name="clear")
     async def _clear(self, ctx):
         await self._stop(ctx)
-    
+
 def setup(bot: discord.Bot):
     bot.add_cog(Music(bot))
